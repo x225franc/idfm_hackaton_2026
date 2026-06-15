0e919203abbb61284e591c93de08033d1cc56a57
Mijosed <mijosed@gmail.com>
2026-06-15 15:26:46 +0200

    phpmyadmin et adminer

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.fje8qo7pg6"
+    "revision": "0.l16rqja2rb"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
