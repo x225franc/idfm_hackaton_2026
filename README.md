e1795202d7126746e58e9dfe4a73612d82d4314e
Alpha Malick DIAWARA <102668503+x225franc@users.noreply.github.com>
2026-06-16 11:29:51 +0200

    Add prod docker stack and DB schema

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # projet idfm_hackaton_2026
+
+docker swarm
+-docker swarm init
+-docker stack deploy -c docker-compose-prod.yml idfm_app
--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.l16rqja2rb"
+    "revision": "0.6c6qle63f1o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
