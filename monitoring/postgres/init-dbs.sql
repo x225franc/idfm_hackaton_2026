af761b819bb6ae2a1442d0a7eaf2fdb350cc43dd
Mijosed <mijosed@gmail.com>
2026-06-17 15:48:13 +0200

    supp monitoring

--- monitoring/postgres/init-dbs.sql
+++
@@ -1,8 +0,0 @@
--- Création des bases de données pour Umami et GlitchTip
--- Ce script est exécuté automatiquement au premier démarrage du conteneur postgres.
-
-CREATE DATABASE umami;
-GRANT ALL PRIVILEGES ON DATABASE umami TO monitoring;
-
-CREATE DATABASE glitchtip;
-GRANT ALL PRIVILEGES ON DATABASE glitchtip TO monitoring;
